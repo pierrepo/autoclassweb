69e766cf69af6c422ce42b53ab95f8949cd09962
Pierre Poulain <pierre.poulain@cupnet.net>
2017-08-03 23:07:03 +0200

    Update model for job management

--- autoclassweb/__init__.py
+++ autoclassweb/__init__.py
@@ -21,19 +21,17 @@ def ping_pong():
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
+    print("We are in: {}".format(os.getcwd()))
+    print("Flask is in : {}".format(os.environ['FLASK_HOME']))
+    os.chdir(os.environ['FLASK_HOME'])
+    print("We are in: {}".format(os.getcwd()))
+
     # create form 
     input_form = forms.InputDataUpload()
 
-
     # list current jobs (running and completed)
-    job_manager = model.JobManager(app.config['UPLOAD_FOLDER'], 4)
+    job_manager = model.JobManager(app.config['UPLOAD_FOLDER'], 4, alive=4)
     job_manager.autodiscover()
-    jobs={"running": [], "completed": [], "max": 4}
-    for job in job_manager.jobs:
-        if job.is_running:
-            jobs["running"].append(job.name)
-        else:
-            jobs["completed"].append(job.name)
 
     # handle form data after POST
     # flash(input_form.errors)
@@ -41,32 +39,36 @@ def index():
         print("input form validated!")
         # get job name and create directory
         name = input_form.job_name.data
-        job = model.Job(app.config['UPLOAD_FOLDER'], name=name)
-        print(job.folder, job.name)
+        job = model.Job()
+        job.create_from_name(app.config['UPLOAD_FOLDER'], name)
+        print(job.path, job.name)
         # get scalar input data
         filename = secure_filename(input_form.scalar_input_file.data.filename)
-        input_form.scalar_input_file.data.save(os.path.join(job.root, job.folder, filename))
+        input_form.scalar_input_file.data.save(os.path.join(job.path, filename))
         scalar = {}
         scalar['file'] = filename
         scalar['header'] = input_form.scalar_has_header.data
         scalar['error'] = input_form.scalar_error.data
         # prepare data to be stored in session
         session['job_name'] = job.name
+        session['job_path'] = job.path
         session['scalar'] = scalar
         return redirect(url_for('startjob'))
 
-    return render_template('index.html', form=input_form, jobs=jobs)
+    return render_template('index.html', form=input_form, job_m=job_manager)
 
 
 @app.route('/startjob', methods=['GET'])
 def startjob():
     if 'job_name' in session:
-        name = session['job_name']
-        job = model.Job(app.config['UPLOAD_FOLDER'], name=name)
+        job_name = session['job_name']
+        job_path = session['job_path']
         scalar = session['scalar']
-        full_path = os.path.join(app.config['UPLOAD_FOLDER'], job.folder)
-        scalar_input = io.AutoclassInput('scalar', full_path, scalar['file'], scalar['header'], scalar['error'])
-        return render_template('startjob.html', msg=scalar_input.log.msg.split('\n'))
+        scalar_clust= autoclass.Autoclass('scalar', job_path, scalar['file'], scalar['header'], scalar['error'])
+        scalar_clust.prepare_input_files()
+        scalar_clust.run()
+        scalar_clust.set_access_token()
+        return render_template('startjob.html', msg=scalar_clust.log.msg.split('\n'))
         #return "Create a new job {} in {} with param {}".format(job.name, job.folder, scalar)
     else:
         return "No job found!"
@@ -74,7 +76,12 @@ def startjob():
 
 @app.route('/status', methods=['GET'])
 def status():
-    job_manager = model.JobManager(app.config['UPLOAD_FOLDER'], 4)
+    print("We are in: {}".format(os.getcwd()))
+    print("Flask is in : {}".format(os.environ['FLASK_HOME']))
+    os.chdir(os.environ['FLASK_HOME'])
+    print("We are in: {}".format(os.getcwd()))
+
+    job_manager = model.JobManager(app.config['UPLOAD_FOLDER'], 4, alive=4)
     job_manager.autodiscover()
 
-    return render_template('status.html', jobs=job_manager.jobs)
+    return render_template('status.html', job_m=job_manager)
--- autoclassweb/model.py
+++ autoclassweb/model.py
@@ -1,65 +1,99 @@
 import os
 import datetime
 import re
-
+from pathlib import Path
 
 class Job():
     """
     Autoclass job management
     """
-    def __init__(self, root, folder=None, name=None):
+    def __init__(self, alive=30):
         """
         Constructor
         """
-        self.root = root
-        self.folder = folder
-        self.name = name
+        self.path = None
+        self.folder = None
+        self.name = None
         self.ctime = None
-        self.mtime = None
-        self.rtime = None 
+        self.is_running = False
+        self.running_time = 0
+        self.alive = alive
+        self.token = ''
 
-        # you need either path or name
-        if self.folder == None and self.name == None:
-            print("Either path or name must be specified for Job instance.")
-            raise
+    def create_from_path(self, path):
+        """
+        Create Job from path
+        """
+        self.path = path
+        answer = self.verify_folder_name(self.path)
+        if answer:
+            self.folder, self.name, self.ctime = answer
+        else:
+            raise("{} is not a valid Job path".format(self.path))
+        # get running status
+        self.test_is_running()
+        # get access token if any
+        self.find_access()
 
-        # create folder from name
 
+    def create_from_name(self, root, name):
+        """
+        Create Job from root directory and job name
+        """
         # clean name
-        if self.name:
-            name_clean = ""
-            for character in name:
-                if character in "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ_-":
-                    name_clean += character
-                else:
-                    name_clean += "-"
-            self.name = name_clean
+        name_clean = ""
+        for character in name:
+            if character in "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMONPQRSTUVWXYZ_-":
+                name_clean += character
+            else:
+                name_clean += "-"
+        self.name = name_clean
 
-            self.ctime = datetime.datetime.now()
-            now_string = datetime.datetime.strftime(self.ctime, "%Y%m%d.%H%M%S")
+        self.ctime = datetime.datetime.now()
+        date = datetime.datetime.strftime(self.ctime, "%Y%m%d")
+        time = datetime.datetime.strftime(self.ctime, "%H%M%S")
 
-            self.folder = "{}.{}".format(now_string, self.name)
-            try:
-                os.makedirs(os.path.join(self.root, self.folder))
-                print("Created folder {}".format(self.folder))
-            except OSError:
-                print("Directory {} already exists".format(self.folder))
-            except:
-                print("Cannot create folder: {}".format(self.folder))
-                raise 
+        self.folder =  "{}.{}.{}".format(date, time, self.name)
+        self.path = os.path.join(root, self.folder)
+        try:
+            os.makedirs(self.path)
+            print("Created folder {}".format(self.folder))
+        except OSError:
+            print("Directory {} already exists".format(self.folder))
+        except:
+            print("Cannot create folder: {}".format(self.folder))
+            raise 
 
-        # create name and ctime from folder
-        if self.folder:
-            self.name, self.ctime = self.verify_folder_name(self.folder)
 
-        # modified time
-        mtime = os.path.getmtime(os.path.join(self.root, self.folder))
-        self.mtime = datetime.datetime.fromtimestamp(mtime)
+    def test_is_running(self):
+        """
+        Test if job is running and for how long
+        """
+        path = Path(self.path)
+        try:
+            log_file = [str(x) for x in path.iterdir() if ".log" in str(x)][0]
+            # modified time
+            mtimestamp = os.path.getmtime(log_file)
+            mtime = datetime.datetime.fromtimestamp(mtimestamp)
 
-        # running status
-        self.is_running = True if ((datetime.datetime.now() - self.mtime).seconds < 30 ) else False
-        # running time
-        self.rtime = (self.mtime - self.ctime).seconds / 60
+            # running status
+            self.is_running = True if ((datetime.datetime.now() - mtime).seconds < self.alive ) else False
+            # running time
+            self.running_time = (mtime - self.ctime).seconds / 60
+        except:
+            self.running_time = 0
+            self.is_running = False
+
+
+    def find_access(self):
+        """
+        Find access token
+        """
+        access_name = os.path.join(self.path, 'access')
+        if os.path.exists(access_name):
+            with open(access_name, 'r') as access_file:
+                self.token = access_file.readline()
+
 
     def __repr__(self):
         """
@@ -74,15 +108,18 @@ class Job():
         """
         Verify folder name is compliant with naming convention
         """
-        separator = "."
-        fields = folder.split(separator)
-        try:
-            date_mask = "%Y%m%d{}%H%M%S".format(separator)
-            ctime = datetime.datetime.strptime(separator.join(fields[0:2]), date_mask)
-            job_name = fields[2]
-            return [job_name, ctime]
-        except:
-            return False
+        regex = re.compile("^(\w+)/([0-9]{8})\.([0-9]{6})\.(\w+)$")
+        find = regex.search(folder)
+        if find:
+            root = find.group(1)
+            date = "{}.{}".format( find.group(2), find.group(3) )
+            name = find.group(4)
+            folder = "{}.{}".format( date, name )
+            try:
+                ctime = datetime.datetime.strptime(date, "%Y%m%d.%H%M%S")
+                return (folder, name, ctime)
+            except:
+                return False
 
 
 
@@ -91,33 +128,43 @@ class JobManager():
     """
     Manager for autoclass jobs
     """
-    def __init__(self, path, max_jobs):
+
+    def __init__(self, path, max_jobs, alive=30):
         """
         Constructor
         """
         self.path = path
-        self.max_jobs = max_jobs
-        self.jobs = []
-        self.alive_threshold = []
+        self.max = max_jobs
+        self.running = []
+        self.completed = []
+        self.alive = alive
+
 
     def autodiscover(self):
         """
         Discover autoclass jobs automatically
         """
         # list sub folders
-        print(os.walk(self.path))
-        job_folder_lst = next(os.walk(self.path))[1]
+        #print(os.walk(self.path))
+
+        #job_folder_lst = next(os.walk(self.path))[1]
+        p = Path(self.path)
+        job_folder_lst = [str(x) for x in p.iterdir() if x.is_dir()]
         
         # create jobs
         for job_folder in job_folder_lst:
             if Job.verify_folder_name(job_folder):
-                job = Job(self.path, folder=job_folder)
-                self.jobs.append(job)
+                job = Job(alive=self.alive)
+                job.create_from_path(job_folder)
+                if job.is_running:
+                    self.running.append(job)
+                else:
+                    self.completed.append(job)
+
 
 
 
 if __name__ == '__main__':
-
 
     job_manager = JobManager("tmp", 4)
     job_manager.autodiscover()

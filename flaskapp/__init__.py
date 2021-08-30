1e7e381d0cc828c41749b9484453532375c88f33
Pierre Poulain <pierre.poulain@cupnet.net>
2021-08-30 09:57:17 +0200

    Bump version: 2.1.1 → 2.2.0

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -48,7 +48,7 @@ else:
 # config module must be loaded after user config file
 import config
 app.config.from_object(config.CreateConfig)
-app.config["VERSION"] = "2.1.1"
+app.config["VERSION"] = "2.2.0"
 app.logger.info(f"AutoClassWeb version: {app.config['VERSION']}")
 
 
